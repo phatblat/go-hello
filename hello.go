d7a6c9255e05b23a5ed8d278345527280106a584
Ben Chatelain <ben@octop.ad>
2021-12-10 16:02:43 -0700

    ✨ Pass name to Hello

--- hello.go
+++ hello.go
@@ -16,8 +16,7 @@ func main() {
     log.SetFlags(0)
 
     // Get a greeting message and print it.
-    // message := greetings.Hello("Gladys")
-    message, err := greetings.Hello("")
+    message, err := greetings.Hello("Gladys")
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
